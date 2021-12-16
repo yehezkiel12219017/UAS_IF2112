ec908b40e7207e72142f84d3b6fb64ff471e250b
yehezkiel12219017 <96163775+yehezkiel12219017@users.noreply.github.com>
2021-12-16 18:13:59 +0700

    Update fix.py

--- fix.py
+++ fix.py
@@ -61,3 +61,46 @@ dataC_graph=px.bar(
 )
 st.plotly_chart(dataC_graph)
 
+##Informasi 
+
+st.markdown('Negara dengan jumlah produksi terbesar')
+selectTahun2 = st.selectbox('Pilih Tahun ', selectorTahun)
+dataD = data[data['tahun'] == selectTahun2]
+dataD=dataD.sort_values(["produksi"],ascending=[0])
+dataD=dataD[:1]
+dataD[["name","kode_negara","region","sub-region","produksi"]]
+
+st.markdown('Negara dengan jumlah produksi terkecil')
+selectTahun3 = st.selectbox('Pilih Tahun  ', selectorTahun)
+dataE = data[data['tahun'] == selectTahun3]
+dataE=dataE.sort_values(["produksi"],ascending=[1])
+dataE= dataE.loc[dataE["produksi"]>0]
+dataE=dataE[:1]
+dataE[["name","kode_negara","region","sub-region","produksi"]]
+
+st.markdown('Negara dengan jumlah produksi nol')
+selectTahun4 = st.selectbox('Pilih Tahun   ', selectorTahun)
+dataF = data[data['tahun'] == selectTahun4]
+dataF=dataF.sort_values(["produksi"],ascending=[1])
+dataF= dataF.loc[dataF["produksi"]==0]
+dataF[["name","kode_negara","region","sub-region"]]
+
+st.markdown('Negara dengan jumlah produksi terbesar kumulatif')
+dataG = data.groupby(["name"])["produksi"].sum().reset_index()
+dataG=dataG.sort_values(["produksi"],ascending=[0])
+dataTemp=data
+dataTemp.drop("produksi", axis=1, inplace=True)
+dataG=pd.merge(dataG,dataTemp,on='name')
+dataG=dataG.drop_duplicates("name")
+dataG[:1][["name","kode_negara","region","sub-region","produksi"]]
+
+st.markdown('Negara dengan jumlah produksi terkecil kumulatif')
+dataH=dataG.sort_values(["produksi"],ascending=[1])
+dataH=dataH.loc[dataH["produksi"]>0]
+dataH=dataH[:1]
+dataH[["name","kode_negara","region","sub-region","produksi"]]
+
+st.markdown('Negara dengan jumlah produksi nol kumulatif')
+dataI=dataG.sort_values(["produksi"],ascending=[1])
+dataI=dataI.loc[dataI["produksi"]==0]
+dataI[["name","kode_negara","region","sub-region"]]
